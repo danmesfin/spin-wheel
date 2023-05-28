7a065320a04e0b96011ec989e4e5f3e0dd8203ba
danmesfin <danielmsfn@gmail.com>
2023-05-29 02:24:53 +0300

    updated ...

---
+++ pages/components/spin-wheel/index.tsx
@@ -0,0 +1,10 @@
+export default function index() {
+  return (
+    <div
+      className={`absolute inset-0 bg-center bg-no-repeat bg-contain `}
+      style={{
+        backgroundImage: `url('/spin-wheel.png')`,
+      }}
+    ></div>
+  );
+}
